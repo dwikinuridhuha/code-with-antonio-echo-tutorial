2787c107b3d3642fa1f3757d3da2100de2c3d2ce
Dwiki Nuri Dhuha <dwiki.nuridhuha@gmail.com>
2025-09-05 10:26:41 +0700

    01:17:11

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,10 +4,15 @@ import { useQuery } from "convex/react";
 import { api } from "@workspace/backend/convex/_generated/api";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello app/web</h1>
+
+        <div className="max-w-sm w-full mx-auto">
+          {JSON.stringify(users, null, 2)}
+        </div>
       </div>
     </div>
   )
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,15 +1,18 @@
-import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math/add"
-import { Input } from "@workspace/ui/components/input"
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "@workspace/backend/convex/_generated/api";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello app/web</h1>
-        <Button size="sm">Button</Button>
-        {add(2, 3)}
-        <Input />
+        <h1 className="text-2xl font-bold">Hello app/widget</h1>
+
+        <div className="max-w-sm w-full mx-auto">
+          {JSON.stringify(users, null, 2)}
+        </div>
       </div>
     </div>
   )
